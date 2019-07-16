816fdfbc9f688942ed3cdef76118b5677412a867
Severin Denk <severin.denk@ipp.mpg.de>
2019-07-16 12:45:08 +0200

    modified:   build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ else if ($SYS == "amd64_sles12") then
   module load intel/17.0
   mpdule load git
 else if ($SYS == "amd64_sles15") then
-  module load intel/19.0.1
+  module load intel/19.0.3
   module load git
 endif
 rm id
