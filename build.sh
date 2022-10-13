27de2f6225b66b7c134be982a485fa58b0667e99
Severin <denks@fusion.gat.com>
2022-10-13 14:11:43 -0700

    Set F90, F77, FC environment variables to gfortran

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-$PYTHON -m build -n -x
-$PYTHON -m pip install --no-deps .
+export F90=gfortran
+export FC=gfortran
+export F77=gfortran
 make clean # Just for good measure
 cd fitpack
 make clean
@@ -15,6 +16,8 @@ make OPEN_MP=True
 make OPEN_MP=True DEBUG=True
 rm id
 git rev-parse HEAD > id
+$PYTHON -m build -n -x
+$PYTHON -m pip install --no-deps .
 # make USE_3D=True DEBUG=True
 # make USE_3D=True
 # make USE_3D=True DEBUG=True OPEN_MP=True
