b9e7bdde5aee758b6d74d90e042cdd7f55da040b
Severin Denk <denks@fusion.gat.com>
2023-07-11 11:33:16 -0700

    Added muscle3 specific build recipe

--- build_msucle3.sh
+++ build_msucle3.sh
@@ -5,8 +5,6 @@ make clean -C odepack
 
 make -C fitpack
 make -C odepack
-make OPEN_MP=True IMAS=True
 make OPEN_MP=True MUSCLE3=True
 make OPEN_MP=True
 make
-iwrap -f iwrap/ecrad.yaml -i ~/public/PYTHON_ACTORS
