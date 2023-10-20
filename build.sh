0c4b9a558c9c3023f724d392172c7e678d54b395
AreWeDreaming <denks@fusion.gat.com>
2023-10-20 14:11:28 -0700

    Hadcoded python3, updated version

--- build.sh
+++ build.sh
@@ -16,8 +16,8 @@ make lib COMPILER=GNU
 make lib OPEN_MP=True COMPILER=GNU
 make lib OPEN_MP=True DEBUG=True COMPILER=GNU
 rm -f id
-$PYTHON -m build -n -x
-$PYTHON -m pip install --no-deps .
+python3 -m build -n -x
+python3 -m pip install --no-deps .
 # make USE_3D=True DEBUG=True
 # make USE_3D=True
 # make USE_3D=True DEBUG=True OPEN_MP=True
